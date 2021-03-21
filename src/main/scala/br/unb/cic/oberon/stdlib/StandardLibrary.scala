4523f64d40524c5fe9e1c2eca3a82d5596655e89
rafaelnogalha <rafaelnogalha@gmail.com>
2021-03-20 21:31:26 -0300

    standard library second version

--- src/main/scala/br/unb/cic/oberon/stdlib/StandardLibrary.scala
+++ src/main/scala/br/unb/cic/oberon/stdlib/StandardLibrary.scala
@@ -5,14 +5,20 @@ import br.unb.cic.oberon.util.Values
 
 object Functions {
     def abs(x: Int): Int = Math.abs(x)
-    def odd(x: Int): Boolean =
-        if (x % 2 == 1)
-            true
-        else
-            false
+    def odd(x: Int): Boolean = x%2 == 1
+    def lsl(x: Int, n:Int): Int = x << n
+    def asr(x: Int, n:Int): Int = x >> n
+    def ror(x: Int, n: Int): Int = {
+        y := x
+        y >> n
+        z := x
+        z << (32-n)
+        return y | z
+    } 
 }
 
 object StandardLibrary {
+
     def abs = Procedure(
         "ABS", // name
         List(FormalArg("x", IntegerType)), // arguments
@@ -44,6 +50,72 @@ object StandardLibrary {
             env.setLocalVariable(Values.ReturnKeyWord, value)
         })
     )
+
+    def len = Procedure(
+        "LEN", // name
+        List(FormalArg("v", ArrayType)), // arguments
+        Some(IntegerType), // return type
+        List(),
+        List(),
+
+        ScalaStmt(env => {
+            val value = (env.lookup("v")) match {
+                case Some(ArrayValue(v)) => IntValue(v.length)
+                case _ => ???
+            }
+            env.setLocalVariable(Values.ReturnKeyWord, value)
+        })
+    )
+
+    def lsl = Procedure(
+        "LSL", // name
+        List(FormalArg("x", IntegerType), FormalArg("n", IntegerType)), // arguments
+        Some(IntegerType), // return type
+        List(),
+        List(),
+
+        ScalaStmt(env => {
+            val value = (env.lookup("x"), env.lookup("n")) match {
+                case Some(IntValue(x)),Some(IntValue(n)) => IntValue(Functions.lsl(x,n))
+                case _ => ???
+            }
+            env.setLocalVariable(Values.ReturnKeyWord, value)
+        })
+    )
+
+    def asr = Procedure(
+        "ASR", // name
+        List(FormalArg("x", IntegerType), FormalArg("n", IntegerType)), // arguments
+        Some(IntegerType), // return type
+        List(),
+        List(),
+
+        ScalaStmt(env => {
+            val value = (env.lookup("x"), env.lookup("n")) match {
+                case Some(IntValue(x)),Some(IntValue(n)) => IntValue(Functions.asr(x,n))
+                case _ => ???
+            }
+            env.setLocalVariable(Values.ReturnKeyWord, value)
+        })
+    )
+
+    def ror = Procedure(
+        "ROR", // name
+        List(FormalArg("x", IntegerType), FormalArg("n", IntegerType)), // arguments
+        Some(IntegerType), // return type
+        List(),
+        List(),
+
+        ScalaStmt(env => {
+            val value = (env.lookup("x"), env.lookup("n")) match {
+                case Some(IntValue(x)),Some(IntValue(n)) => IntValue(Functions.ror(x,n))
+                case _ => ???
+            }
+            env.setLocalVariable(Values.ReturnKeyWord, value)
+        })
+    )
+
+
 
     val procedures = Map(
         abs.name -> abs,
